21e094ebf2b7e316be67b676344d48ae26a92166
asif2212 <asifmd2212@gamil.com>
2023-08-21 01:41:04 +0600

    table design ready

---
+++ resources/views/products.blade.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
+
+    <title>Laravel Ajax Crud</title>
+  </head>
+  <body>
+   <div class="container">
+    <div class="row">
+        <div class="col-md-8 m-auto">
+            <h2 class="text-center my-3">Laravel Ajax Crud Operation</h2>
+            <table class="table table-striped">
+                <thead>
+                  <tr>
+                    <th scope="col">SL. No.</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Price</th>
+                    <th scope="col">Action</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>
+                        <a class="btn btn-success" href=""><i class="las la-edit"></i></a>
+                        <a class="btn btn-danger" href=""><i class="las la-trash"></i></a>
+                    </td>
+                  </tr>
+                 
+                </tbody>
+              </table>
+        </div>
+    </div>
+   </div>
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+  </body>
+</html>
