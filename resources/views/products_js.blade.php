47504456664355d12de550cdddd4934b7975c742
asif2212 <asifmd2212@gamil.com>
2023-08-21 01:50:02 +0600

    setup ajax csrf token and jquery testing

---
+++ resources/views/products_js.blade.php
@@ -0,0 +1,16 @@
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
+<script>
+    
+    $.ajaxSetup({
+headers: {
+    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+}
+});
+
+</script>
+<script>
+    $(document).ready(function(){
+alert();
+})
+</script>
